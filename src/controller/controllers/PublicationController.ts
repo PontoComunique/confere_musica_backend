46350154ebacef52004aa309a4eaa4b2204ddaac
jschneiderm98 <julio_schneider@hotmail.com>
2021-09-18 12:11:35 -0300

    feat: update image on publication update

--- src/controller/controllers/PublicationController.ts
+++ src/controller/controllers/PublicationController.ts
@@ -1,6 +1,6 @@
 import type { RequestHandler } from 'express'
 import { v4 as uuidv4 } from 'uuid'
-import { createPublication, updatePublication, deletePublication, readPublication, readPublications } from '../../model/PublicationModel'
+import { createPublication, updatePublication, deletePublication, readPublication, readPublications, readPublicationImage } from '../../model/PublicationModel'
 import * as ImageHandler from '../../utils/imageHandler'
 import { AmazonStorage } from '../../utils/AmazonStorage'
 import removeFile from '../../utils/removeFile'
@@ -32,7 +32,17 @@ const PublicationController: Controller = {
     const { id } = req.params
     const { title, author, content } = req.body
     try {
-      await updatePublication(id, title, author, content, new Date())
+      if (req.file) {
+        const data = await readPublicationImage(id)
+        if (data.length < 1) { return View.NotFound(res, 'No Publication with that id') }
+        await ImageHandler.deleteImage(data[0].storageKey, new AmazonStorage())
+        await ImageHandler.pushImage(req.file, new AmazonStorage())
+        await updatePublication(id, title, author, content, new Date(), req.file.filename)
+        await removeFile(req.file.path)
+      } else {
+        const data = await updatePublication(id, title, author, content, new Date())
+        if (data.length < 1) { return View.NotFound(res, 'No Publication with that id') }
+      }
       View.Success(res, 'Publication updated!')
     } catch (error) {
       View.InternalServerError(res)
--- src/model/PublicationModel.ts
+++ src/model/PublicationModel.ts
@@ -7,8 +7,8 @@ export async function createPublication (id: string, title: string, author: stri
   return data
 }
 
-export async function updatePublication (id: string, title: string, author: string, content: string, createdAt: Date): Promise<any> {
-  const data = await Connection(tableName).where({ id }).update({ title, author, content, createdAt })
+export async function updatePublication (id: string, title: string, author: string, content: string, createdAt: Date, storageKey?: string): Promise<any> {
+  const data = storageKey ? await Connection(tableName).where({ id }).update({ title, author, content, createdAt, storageKey }, 'id') : await Connection(tableName).where({ id }).update({ title, author, content, createdAt }, 'id')
   return data
 }
 
@@ -16,8 +16,14 @@ export async function deletePublication (id: string): Promise<any> {
   const data = await Connection(tableName).where({ id }).delete().returning('storageKey')
   return data
 }
+
 export async function readPublication (id: string): Promise<any[]> {
   const data = await Connection(tableName).where({ id })
+  return data
+}
+
+export async function readPublicationImage (id: string): Promise<any[]> {
+  const data = await Connection(tableName).select('storageKey').where({ id })
   return data
 }
 
